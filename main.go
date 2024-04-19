9e4b2677d0f092921396736374add9d6fc77857d
xuzhenxing <xrnmail@163.com>
2024-04-19 14:28:01 +0800

    Add reply downlink message from exec command

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
     "fmt"
+    "strings"
     "os"
     "os/signal"
+    "os/exec"
     "time"
 
     "github.com/eclipse/paho.mqtt.golang"
@@ -11,22 +13,33 @@ import (
 
 const (
     MQTT_URL = "tcp://localhost:1883"
-    MQTT_TOPIC_UPLOAD = "topic/upload"
-    MQTT_TOPIC_DOWNLOAD = "topic/download"
+    MQTT_TOPIC_UPLINK = "topic/uplink"
+    MQTT_TOPIC_DOWNLINK = "topic/downlink"
+)
+
+const (
+    EXEC_CMD = "uname"
+    EXEC_CMD_ARGS = "-a"
 )
 
 // Define message handler
 func message_handler(client mqtt.Client, msg mqtt.Message) {
-    fmt.Printf("Upload Topic: %s, Payload: %s\n", msg.Topic(), msg.Payload())
+    fmt.Printf("Upload payload: %s\n", msg.Payload())
 
     // Reply download message
-    token := client.Publish(MQTT_TOPIC_DOWNLOAD, 0, true, "Reply download message.")
-    token.Wait()
+    out, err := exec.Command(EXEC_CMD, EXEC_CMD_ARGS).Output()
+    if err != nil {
+        token := client.Publish(MQTT_TOPIC_DOWNLINK, 0, true, err)
+        token.Wait()
+    } else {
+	    token := client.Publish(MQTT_TOPIC_DOWNLINK, 0, true, strings.TrimSpace(string(out)))
+        token.Wait()
+    }
     // TODO: It can also realize the control on hardware.
 }
 
 func listen(client mqtt.Client) {
-    if token := client.Subscribe(MQTT_TOPIC_UPLOAD, 1, message_handler); token.Wait() && token.Error() != nil {
+    if token := client.Subscribe(MQTT_TOPIC_UPLINK, 1, message_handler); token.Wait() && token.Error() != nil {
         panic(token.Error())
     }
     time.Sleep(100 * time.Millisecond)
@@ -52,9 +65,9 @@ func main() {
     // Waiting interrupt signal
     <-sig_chan
 
-    token := client.Publish(MQTT_TOPIC_DOWNLOAD, 0, true, "")
+    token := client.Publish(MQTT_TOPIC_DOWNLINK, 0, true, "")
     token.Wait()
 
-    fmt.Printf("\n\033[0;33mReceived interrupt signal and disconnecting...\033[0m\n")
+    fmt.Printf("\n\033[1;33mReceived interrupt signal and disconnecting...\033[0m\n")
     client.Disconnect(250)
 }
